9a7afd8308b676f26b89f2c617413e11a949058b
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2021-10-02 11:13:46 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -1,39 +1,55 @@
 class DataModel {
-    constructor() {
-        this.data = [];
-        this.errors = [];
+  constructor() {
+    this.data = []
+    this.errors = []
+  }
+
+  getAll() {
+    return this.data
+  }
+
+  getById(id) {
+    let user = this.data.find((obj) => obj.id === id)
+    if (!user) {
+      return null
     }
+    return user
+  }
 
-    getAll() {
-        return this.data;
+  save(obj) {
+    if (this.validate(obj)) {
+      this.data.push(obj)
+      return true
     }
+    return false
+  }
 
-    getById(id) {
-
+  update(obj, id) {
+    let user = this.data.find((item) => item.id === id)
+    if (!user) {
+      return false
     }
-
-    save(obj) {
-        if (this.validate(obj)) {
-            this.data.push(obj);
-            return true;
-        }
-        return false;
+    for (let key in obj) {
+      user[key] = obj[key]
     }
+    return true
+  }
 
-    update(obj, id) {
-
+  delete(id) {
+    let index = this.data.findIndex((item) => item.id === id)
+    if (index > -1) {
+      this.data.splice(index, 1)
+      return true
     }
+    return false
+  }
 
-    delete(id) {
-
-    }
-
-    // this method will be overriden in the sub classes
-    validate(obj) {
-        return false;
-    }
+  // this method will be overriden in the sub classes
+  validate(obj) {
+    return false
+  }
 }
 
 // Do not worry about the below for now; It is included so that we can test your code
 // We will cover module exports in later parts of this course
-module.exports = DataModel;
+module.exports = DataModel
